4ba800082f46fcaf6b8a5fbe663fe8b8e7707ae7
bananagif <whanlon34@yahoo.com>
2015-09-12 11:03:54 +0800

    Delete theGame.js

--- theGame.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#lol").click(function(){
-		$("#wrapperID").toggleClass("secondWrapper");
-		$("#wrapperID").toggleClass("wrapper");
-		$("#lol").toggleClass("hidden");
-    });
-});
